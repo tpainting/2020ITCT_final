886596e3a9c54388e2ccf26f61558df532dbb0ac
kevin47 <kevinwang0810@gmail.com>
2020-06-10 17:43:08 +0800

    Let's GOOOOOO

--- break_permutation.py
+++ break_permutation.py
@@ -5,114 +5,10 @@ import numpy as np
 import cv2
 import sys
 import random
-#import encrypt
+from encrypt import Encrypt
 
 up_ans = []
 vp_ans = []
-
-def get_R(A):
-    # Step 1
-    (m, n) = A.shape
-    s = Entropy(A)
-
-    x_0, y_0 = UpdateKey1(x0, y0, xp0, yp0, s)
-    P_seq = LASM2D(mu, x_0, y_0, m*n)
-    P = P_seq.reshape(A.shape)
-
-    # Step 2
-    a = np.ceil((x0+y0+1)*(10**7)) % (m)
-    b = np.ceil((xp0+yp0+2)*(10**7)) % (n)
-    u = P[int(a), :]
-    v = P[:, int(b)]
-    up = np.ceil(u*(10**14)) % (n)
-    vp = np.ceil(v*(10**14)) % (m)
-    up = up.astype(int)
-    vp = vp.astype(int)
-    Uniq(up)
-    Uniq(vp)
-    B = np.zeros(A.shape, dtype='uint8')
-    tmp = np.zeros(A.shape, dtype='uint8')
-    for i in range(n):
-        tmp[:, up[i]] = A[:, i]
-    for i in range(m):
-        B[vp[i], :] = tmp[i, :]
-    
-    # Step 3
-    W = np.zeros(A.shape, dtype='uint8')
-    for i in range(m):
-        for j in range(n):
-            W[i][j] = (m*n+(i+1)+(j+1)) % 256
-    R = (B+W) % 256
-    return R
-
-
-def Encrypt(A):
-    # Step 1
-    (m, n) = A.shape
-    s = Entropy(A)
-    #print('s: %.40f\n' % s)
-
-    x_0, y_0 = UpdateKey1(x0, y0, xp0, yp0, s)
-    P_seq = LASM2D(mu, x_0, y_0, m*n)
-    P = P_seq.reshape(A.shape)
-
-    # Step 2
-    a = np.ceil((x0+y0+1)*(10**7)) % (m)
-    b = np.ceil((xp0+yp0+2)*(10**7)) % (n)
-    u = P[int(a), :]
-    v = P[:, int(b)]
-    up = np.ceil(u*(10**14)) % (n)
-    vp = np.ceil(v*(10**14)) % (m)
-    up = up.astype(int)
-    vp = vp.astype(int)
-    Uniq(up)
-    Uniq(vp)
-    B = np.zeros(A.shape, dtype='uint8')
-    tmp = np.zeros(A.shape, dtype='uint8')
-    for i in range(n):
-        tmp[:, up[i]] = A[:, i]
-    for i in range(m):
-        B[vp[i], :] = tmp[i, :]
-    
-    # Step 3
-    W = np.zeros(A.shape, dtype='uint8')
-    for i in range(m):
-        for j in range(n):
-            W[i][j] = (m*n+(i+1)+(j+1)) % 256
-    R = (B+W) % 256
-
-    global up_ans, vp_ans
-    if len(up_ans) == 0:
-        up_ans = up
-    elif not all(up_ans == up):
-        print("JIZZZ")
-    if len(vp_ans) == 0:
-        vp_ans = vp
-    elif not all(vp_ans == vp):
-        print("JIZZZ")
-
-
-
-    # Step 4
-    xp_0, yp_0 = UpdateKey2(x0, y0, xp0, yp0)
-    K_seq = LASM2D(mu, xp_0, yp_0, m*n)
-    K = K_seq.reshape(A.shape)
-    K = np.ceil(K*(10**14)) % 256
-    K = K.astype('uint8')
-
-    # Step 5
-    C = np.zeros(A.shape, dtype='uint8')
-    for i in range(n):
-        if i < n-1:
-            d = np.ceil(Entropy(R[:, i+1:])*(10**14)) % n
-            d = d.astype(int)
-        else:
-            d = 0
-        if i == 0:
-            C[:, i] = (R[:, i]+(d+1)*K[:, i]+K[:, d]) % 256
-        else:
-            C[:, i] = (R[:, i]+(d+1)*C[:, i-1]+(d+1)*K[:, i]+K[:, d]) % 256
-    return C
 
 def swap(A, p1, p2):
     T = np.array(A)
@@ -146,8 +42,8 @@ def get_cnt(A):
 
 if __name__ == '__main__':
     random.seed(711222)
-    N = 512
-    M = 512
+    N = 260
+    M = 260
     cnt = np.zeros(256, dtype='int')
     v2p = [[] for i in range(256)]
     A = np.array([[0]*N]*M, dtype='uint8')
